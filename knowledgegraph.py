e123dfb7936ce51d6f1c1ffbebfdefd3fbca457f
Yener <help@ownthink.com>
2018-07-12 21:05:24 +0800

    Update knowledgegraph.py

--- knowledgegraph.py
+++ knowledgegraph.py
@@ -1,11 +1,11 @@
 import requests
 
-url = 'https://api.ownthink.com/kg/knowledge?entity=苹果'
-sess = requests.get(url)
+url = 'https://api.ownthink.com/kg/knowledge?entity=苹果'      # 知识图谱API
+sess = requests.get(url) # 请求
 
-text = sess.text
-print(text)
+text = sess.text # 获取返回的数据
+print(text) 
 
-knowledge = eval(text)
+knowledge = eval(text) # 转为字典类型
 print(knowledge)
-print(knowledge['data']['avp'])
+print(knowledge['data']['avp']) # 打印avp
